17f76080b70833b083f19a1193c58e4b26413b6d
pst <pst@pst-HP>
2015-10-18 12:38:40 +0200

    Added utils classes..

--- iLoC/src/it/cnr/istc/iloc/utils/MathUtils.java
+++ iLoC/src/it/cnr/istc/iloc/utils/MathUtils.java
@@ -1,10 +1,28 @@
+/*
+ * Copyright (C) 2015 Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
 package it.cnr.istc.iloc.utils;
 
 import java.math.BigInteger;
 
 /**
  *
- * @author Riccardo De Benedictis
+ * @author Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
  */
 public class MathUtils {
 
